6ca516b1e700d08184e44e3042f3aafbbdf899fd
tomernst@gmx.net <tomernst@gmx.net>
2019-07-08 02:00:03 +0200

    binaries hinzugefügt

---
+++ binaries/binaries/binaries.cpp
@@ -0,0 +1,12 @@
+﻿// binaries.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "binaries.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
